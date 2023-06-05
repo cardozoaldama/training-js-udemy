32d4f64297039ac337a8c85ca9061ea2d12e2a52
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-05 19:20:07 -0400

    Updated some exercises

--- 01-fundamentos/consoles.js
+++ 01-fundamentos/consoles.js
@@ -1,1 +1,23 @@
 console.log ('Hello there!');
+
+// Trabajando con variables.
+
+let number1 = 25;
+let number2 = 44;
+let number3 = 2;
+let number4 = 788;
+
+let suma = 0;
+let resta = 0;
+let division = 0;
+let multiplicacion = 0;
+
+suma = number1 + number2;
+resta = suma - number1;
+division = suma / resta;
+multiplicacion = division * resta * suma;
+
+console.log(suma);
+console.log(resta);
+console.log(division);
+console.log(multiplicacion);
