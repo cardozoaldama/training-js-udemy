b1ca8dc203a214223c6b6d1ad4fed9693092b314
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-05 18:00:35 -0400

    Add new variables and constants

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,2 +1,20 @@
 console.log('Hola Mundo');
 console.log( console.log('Hola Mundo') );
+
+// Var y Let sirven para crear variables.
+let a = 10,
+    b = 11,
+    c = 12;
+//
+
+// Malas prácticas
+e = 0;
+
+const d = 34;
+// d = 324;
+
+// Más variales
+
+let x = 25;
+x = x + a;
+console.log( x );
