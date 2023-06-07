f64f3dc2122f1e46902658231e05366272de42a2
Fernando Cardozo <nuevo_10000@outlook.com>
2023-06-07 11:15:57 -0400

    New functions such as prompt, alert, confirm

---
+++ 01-fundamentos/assets/js/alert.js
@@ -0,0 +1,10 @@
+// You could use an alert:
+
+let myAlert = 'Some amazing alert';
+alert(myAlert);
+
+let myPrompt = 'Do you have an entry or input?';
+prompt(myPrompt);
+
+let myConfirm = 'Are you sure to leave this place?';
+confirm(myConfirm);
